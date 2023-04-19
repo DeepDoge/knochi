5664f30201c5d410d6f19e7ff250e28b939c136f
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-04-19 12:53:43 +0000

    cool

--- src/libs/wallet-address.ts
+++ src/libs/wallet-address.ts
@@ -1,3 +1,4 @@
+import { Copy2Svg } from "@/assets/svgs/copy2"
 import type { Address } from "@/utils/address"
 import { defineComponent } from "master-ts/library/component"
 import { css, html } from "master-ts/library/template"
@@ -12,6 +13,7 @@ export function WalletAddress(address: Address) {
 			title=${address}
 			aria-label="wallet address, click to copy"
 		>
+			<x ${Copy2Svg()} aria-hidden></x>
 			<span>${address.substring(0, address.length - 4)}</span><span>${address.substring(address.length - 4)}</span>
 		</button>
 	`
@@ -44,14 +46,25 @@ WalletAddressComponent.$css = css`
 
 	button {
 		display: inline-grid;
-		grid-template-columns: 1fr auto;
+		grid-template-areas: "address-start address-end . svg";
+		grid-template-columns: 1fr auto .5ch 1em;
+		align-items: center;
 		max-width: 20ch;
 		color: inherit;
 
-		& > span:first-child {
+		& > svg {
+			grid-area: svg;
+		}
+
+		& > span:nth-child(1 of span) {
+			grid-area: address-start;
 			overflow: hidden;
 			text-overflow: ellipsis;
 			white-space: nowrap;
+		}
+
+		& > span:nth-child(2 of span) {
+			grid-area: address-end;
 		}
 	}
 `
