69c5c4023fe0b9c76ba1a6ac038f53b1884d50ae
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-04-19 20:30:34 +0000

    ...

--- src/utils/ticks.ts
+++ src/utils/ticks.ts
@@ -1,26 +1,31 @@
 import { $ } from "master-ts/library/$"
 
 export const oneSecondTick = $.readable((set) => {
+	set(Date.now())
 	const interval = setInterval(() => set(Date.now()), 1000)
 	return () => clearInterval(interval)
 })
 
 export const tenSecondsTick = $.readable((set) => {
+	set(Date.now())
 	const interval = setInterval(() => set(Date.now()), 10000)
 	return () => clearInterval(interval)
 })
 
 export const oneMinuteTick = $.readable((set) => {
+	set(Date.now())
 	const interval = setInterval(() => set(Date.now()), 60000)
 	return () => clearInterval(interval)
 })
 
 export const tenMinutesTick = $.readable((set) => {
+	set(Date.now())
 	const interval = setInterval(() => set(Date.now()), 600000)
 	return () => clearInterval(interval)
 })
 
 export const oneHourTick = $.readable((set) => {
+	set(Date.now())
 	const interval = setInterval(() => set(Date.now()), 3600000)
 	return () => clearInterval(interval)
 })
