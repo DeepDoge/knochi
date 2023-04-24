2fa2812c907e5ec93b0db2484882cce3b24b9a11
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-04-24 12:55:22 +0000

    better

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,8 +1,10 @@
 import { $ } from "master-ts/library/$"
 import { defineComponent } from "master-ts/library/component"
 import { css, html } from "master-ts/library/template"
-import { ChartSvg } from "./assets/svgs/chart"
-import { HomeSvg } from "./assets/svgs/home"
+import { ChartFilledSvg } from "./assets/svgs/chart-filled"
+import { ChartOutlineSvg } from "./assets/svgs/chart-outline"
+import { HomeFilledSvg } from "./assets/svgs/home-filled"
+import { HomeOutlineSvg } from "./assets/svgs/home-outline"
 import { SearchSvg } from "./assets/svgs/search"
 import { Profile } from "./libs/profile"
 import { route } from "./route"
@@ -13,13 +15,20 @@ export function Navigation() {
 	const component = new NavigationComponent()
 
 	component.$html = html`
-		<x ${Profile($.derive(() => Address("0xE272C9a263701DAFFe940FB4ecEACFa9b2c1217D")))} class="profile"></x>
+		<div class="left">
+			<x
+				${Profile($.derive(() => Address("0xE272C9a263701DAFFe940FB4ecEACFa9b2c1217D")))}
+				class="profile"
+				class:active=${() => route.path.ref.startsWith(Address("0xE272C9a263701DAFFe940FB4ecEACFa9b2c1217D"))}></x>
+		</div>
 
 		<nav>
 			<ul>
 				<li>
 					<a href="#" class:active=${() => route.path.ref === ""} aria-label="home" title="Home">
-						<x ${HomeSvg()} class="icon"></x>
+						${$.match(route.path)
+							.case("", () => html`<x ${HomeFilledSvg()} class="icon"></x>`)
+							.default(() => html`<x ${HomeOutlineSvg()} class="icon"></x>`)}
 					</a>
 				</li>
 				<li>
@@ -29,13 +38,15 @@ export function Navigation() {
 				</li>
 				<li>
 					<a href="#top" class:active=${() => route.path.ref === "top"} aria-label="top posts" title="Top Posts">
-						<x ${ChartSvg()} class="icon"></x>
+						${$.match(route.path)
+							.case("top", () => html`<x ${ChartFilledSvg()} class="icon"></x>`)
+							.default(() => html`<x ${ChartOutlineSvg()} class="icon"></x>`)}
 					</a>
 				</li>
 			</ul>
 		</nav>
 
-		<div class="other">Other</div>
+		<div class="right">Other</div>
 	`
 
 	return component
@@ -58,7 +69,13 @@ NavigationComponent.$css = css`
 		font-size: 0.85em;
 	}
 
-	.other {
+	.left {
+		display: grid;
+		justify-content: start;
+		align-items: center;
+	}
+
+	.right {
 		display: grid;
 		justify-content: end;
 		align-items: center;
@@ -76,25 +93,21 @@ NavigationComponent.$css = css`
 			display: grid;
 			grid-auto-flow: column;
 			align-items: center;
-
-			& + li::before {
-				content: "";
-				border-left: solid hsl(var(--master-hsl)) 1px;
-				padding-left: calc(var(--span) * 1);
-				height: 25%;
-			}
-			&:has(+ *) {
-				padding-right: calc(var(--span) * 1);
-			}
 		}
 
 		& .icon {
 			height: min(1.5em, 5vh);
 			color: hsl(var(--base-text-hsl));
 		}
+	}
 
-		& .active .icon {
-			color: hsl(var(--master-hsl));
+	ul a,
+	.profile {
+		padding: calc(var(--span) * 0.5) calc(var(--span) * 1);
+		border-radius: var(--radius);
+
+		&.active {
+			background-color: hsl(var(--master-text-hsl), 15%);
 		}
 	}
 `
