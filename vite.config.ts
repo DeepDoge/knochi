6d1cc2824b10b987c193f0c5a1c918b298ca72c3
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-04-27 15:56:53 +0000

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { masterTsPlugin } from "master-ts-vite-plugin/plugin"
 import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
-	plugins: [viteSingleFile()],
+	plugins: [masterTsPlugin, viteSingleFile()],
 	build: {
 		target: "esnext",
 	},
