d81c6b264b06faba08be73dcf127b6423126f4f3
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-03 22:44:03 +0000

    ...

--- src/typify-contracts/index.ts
+++ src/typify-contracts/index.ts
@@ -14,12 +14,12 @@ for (const filename of abiFilenames) {
 		throw new Error("Json file is doesn't contain abi.")
 	const abi = jsonObj.abi
 	const type = [
-		`import type { TypifyContract } from "@/typify-contracts/typify"`,
+		`import type { TypifyContract } from "@/typify-contracts/types"`,
 		`import type { Provider } from "@ethersproject/providers"`,
 		`import { Contract, Signer } from "ethers"`,
-		`const abi = ${JSON.stringify(abi, null, "\t")} as const`,
 		`export type ${name}_Contract = TypifyContract<typeof abi> & {}`,
 		`export const connect_${name} = (address: string, signer: Signer | Provider) => new Contract(address, abi, signer) as ${name}_Contract`,
+		`const abi = ${JSON.stringify(abi, null, "\t")} as const`,
 	].join("\n")
 	fs.writeFileSync(path.join(path.dirname(filePath), `${name}.ts`), type)
 }
