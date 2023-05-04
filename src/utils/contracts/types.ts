bb7b039c3dc9c2538fcaefc8a3eb390e8b3bd4a5
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-04 08:00:01 +0000

    reallly cool

--- src/utils/contracts/types.ts
+++ src/utils/contracts/types.ts
@@ -1,39 +1,59 @@
 import type { Address } from "@/utils/address"
-import type { BigNumber, BytesLike, Contract, ContractInterface, ContractTransaction, Overrides } from "ethers"
-import type { Booleans, ComposeLeft, Fn, Objects, Pipe, Tuples } from "hotscript"
+import type { BigNumber, BytesLike, Contract, ContractInterface, ContractTransaction, Overrides as Overrides_ } from "ethers"
+import type { Booleans, ComposeLeft, Fn, Match, Objects, Pipe, Tuples, _ } from "hotscript"
 
 type PromiseOrValue<T> = T | Promise<T>
 
-interface ToType extends Fn {
-	$type: this["arg0"]
-	return: this["$type"] extends `${"u" | ""}int${number | ""}` | `${"u" | ""}fixed`
-		? BigNumber
-		: this["$type"] extends `bytes${number | ""}`
-		? PromiseOrValue<BytesLike>
-		: this["$type"] extends "string"
-		? string
-		: this["$type"] extends "bool"
-		? boolean
-		: this["$type"] extends "address"
-		? Address
-		: unknown
+interface ToPromise extends Fn {
+	return: Promise<this["arg0"]>
 }
 
+interface ToType extends Fn {
+	$type: this["arg0"]
+	return: Pipe<
+		this["$type"],
+		[
+			Match<
+				[
+					Match.With<`${"u" | ""}int${number | ""}` | `${"u" | ""}fixed`, BigNumber>,
+					Match.With<`bytes${number | ""}`, PromiseOrValue<BytesLike>>,
+					Match.With<"string", string>,
+					Match.With<"bool", boolean>,
+					Match.With<"address", Address>,
+					Match.With<_, unknown>
+				]
+			>
+		]
+	>
+}
+
+type Overrides = Overrides_ & { from?: PromiseOrValue<string> } & {}
+
 interface ToFunction extends Fn {
+	$name: this["arg0"]["name"]
 	$inputs: this["arg0"]["inputs"]
 	$outputs: this["arg0"]["outputs"]
 
 	$args: Pipe<this["$inputs"], [Tuples.Map<ComposeLeft<[Objects.Get<"type">, ToType]>>]>
 	return: [
-		this["arg0"]["name"],
+		this["$name"],
 		(
-			...args: readonly [...args: this["$args"], overrides?: Overrides & { from?: PromiseOrValue<string> }]
-		) => Promise<
-			this["$outputs"] extends readonly []
-				? ContractTransaction
-				: this["$outputs"] extends readonly [any]
-				? Pipe<this["arg0"]["outputs"], [Tuples.Map<ComposeLeft<[Objects.Get<"type">, ToType]>>, Tuples.At<0>]>
-				: Pipe<this["arg0"]["outputs"], [Tuples.Map<ComposeLeft<[Objects.Get<"type">, ToType]>>]>
+			...args: readonly [...args: this["$args"], overrides?: Overrides]
+		) => Pipe<
+			this["$outputs"],
+			[
+				Match<
+					[
+						Match.With<readonly [], ContractTransaction>,
+						Match.With<
+							readonly [any],
+							Pipe<this["arg0"]["outputs"], [Tuples.Map<ComposeLeft<[Objects.Get<"type">, ToType]>>, Tuples.At<0>]>
+						>,
+						Match.With<_, Pipe<this["arg0"]["outputs"], [Tuples.Map<ComposeLeft<[Objects.Get<"type">, ToType]>>]>>
+					]
+				>,
+				ToPromise
+			]
 		>
 	]
 }
