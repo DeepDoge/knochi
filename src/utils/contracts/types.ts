210b49fef973df57c164f1bbf4d50520c220bc20
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-04 07:30:38 +0000

    cool

--- src/utils/contracts/types.ts
+++ src/utils/contracts/types.ts
@@ -1,13 +1,15 @@
 import type { Address } from "@/utils/address"
 import type { BigNumber, BytesLike, Contract, ContractInterface, ContractTransaction, Overrides } from "ethers"
-import type { Booleans, Call, Fn, Objects, Pipe, Tuples } from "hotscript"
+import type { Booleans, ComposeLeft, Fn, Objects, Pipe, Tuples } from "hotscript"
+
+type PromiseOrValue<T> = T | Promise<T>
 
 interface ToType extends Fn {
-	$type: this["arg0"]["type"]
+	$type: this["arg0"]
 	return: this["$type"] extends `${"u" | ""}int${number | ""}` | `${"u" | ""}fixed`
 		? BigNumber
 		: this["$type"] extends `bytes${number | ""}`
-		? Promise<BytesLike> | BytesLike
+		? PromiseOrValue<BytesLike>
 		: this["$type"] extends "string"
 		? string
 		: this["$type"] extends "bool"
@@ -21,17 +23,17 @@ interface ToFunction extends Fn {
 	$inputs: this["arg0"]["inputs"]
 	$outputs: this["arg0"]["outputs"]
 
-	$args: Call<Tuples.Map<ToType>, this["$inputs"]>
+	$args: Pipe<this["$inputs"], [Tuples.Map<ComposeLeft<[Objects.Get<"type">, ToType]>>]>
 	return: [
 		this["arg0"]["name"],
 		(
-			...args: readonly [...args: this["$args"], overrides?: Overrides & { from?: Promise<string> | string }]
+			...args: readonly [...args: this["$args"], overrides?: Overrides & { from?: PromiseOrValue<string> }]
 		) => Promise<
 			this["$outputs"] extends readonly []
 				? ContractTransaction
 				: this["$outputs"] extends readonly [any]
-				? Call<Tuples.Map<ToType>, this["arg0"]["outputs"]>[0]
-				: Call<Tuples.Map<ToType>, this["arg0"]["outputs"]>
+				? Pipe<this["arg0"]["outputs"], [Tuples.Map<ComposeLeft<[Objects.Get<"type">, ToType]>>, Tuples.At<0>]>
+				: Pipe<this["arg0"]["outputs"], [Tuples.Map<ComposeLeft<[Objects.Get<"type">, ToType]>>]>
 		>
 	]
 }
