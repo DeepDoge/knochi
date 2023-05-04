3c1c272f60c63a5e10a46fee8d4b3245463f6ebf
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-04 10:54:04 +0000

    ...

--- src/utils/bigmath.ts
+++ src/utils/bigmath.ts
@@ -2,17 +2,20 @@ export namespace BigMath {
 	export function abs(x: bigint) {
 		return x < 0n ? -x : x
 	}
+
 	export function sign(x: bigint) {
-		if (x === 0n) return 0n
-		return x < 0n ? -1n : 1n
+		return x === 0n ? 0n : x < 0n ? -1n : 1n
 	}
+
 	export function pow(base: bigint, exponent: bigint) {
 		return base ** exponent
 	}
+
 	export function min(value: bigint, ...values: bigint[]) {
 		for (const v of values) if (v < value) value = v
 		return value
 	}
+
 	export function max(value: bigint, ...values: bigint[]) {
 		for (const v of values) if (v > value) value = v
 		return value
