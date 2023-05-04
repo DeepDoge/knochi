acdf9aa5248cd3eef8adf6ca4628797a417818ea
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-04 08:39:59 +0000

    better abi types

--- src/utils/contracts/types.ts
+++ src/utils/contracts/types.ts
@@ -1,5 +1,10 @@
-import type { BigNumber, BytesLike, Contract, ContractInterface, ContractTransaction, Overrides as Overrides_ } from "ethers"
+import type { BigNumber, BytesLike, Contract, ContractTransaction, Overrides as Overrides_ } from "ethers"
 import type { Booleans, ComposeLeft, Fn, Match, Objects, Pipe, Tuples, _ } from "hotscript"
+
+export type TypifyContract<TAbi extends Abi> = Contract & {
+	functions: Pipe<TAbi, [Tuples.Filter<Booleans.Extends<{ type: "function" }>>, Tuples.Map<ToFunction>, Tuples.ToUnion, Objects.FromEntries]>
+	// filters: Pipe<TAbi, [Tuples.Filter<Booleans.Extends<{ type: "event" }>>, ]>
+}
 
 type PromiseOrValue<T> = T | Promise<T>
 
@@ -56,7 +61,38 @@ interface ToFunction extends Fn {
 	]
 }
 
-export type TypifyContract<TAbi extends ContractInterface> = Contract & {
-	functions: Pipe<TAbi, [Tuples.Filter<Booleans.Extends<{ type: "function" }>>, Tuples.Map<ToFunction>, Tuples.ToUnion, Objects.FromEntries]>
-	// filters: Pipe<TAbi, [Tuples.Filter<Booleans.Extends<{ type: "event" }>>, ]>
+export type Abi = readonly Abi.Item[]
+export namespace Abi {
+	export type Item = FunctionItem | EventItem
+
+	type Argument = {
+		type: string
+		name: string
+		internalType: string
+	}
+
+	export type FunctionItem = {
+		type: FunctionItem.Type
+		name: string
+		inputs: readonly FunctionItem.Input[]
+		outputs: readonly FunctionItem.Output[]
+		stateMutability: FunctionItem.StateMutability
+	}
+	export namespace FunctionItem {
+		export type Type = "function" | "constructor"
+		export type Input = Argument & {}
+		export type Output = Argument & {}
+		export type StateMutability = "pure" | "view" | "constant" | "nonpayable" | "payable"
+	}
+
+	export type EventItem = {
+		type: EventItem.Type
+		name: string
+		inputs: readonly EventItem.Input[]
+		anonymous: boolean
+	}
+	export namespace EventItem {
+		export type Type = "event"
+		export type Input = Argument & { indexed: boolean }
+	}
 }
