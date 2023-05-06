c31290c2cf5c6e01ec9129f462506fd3145a0317
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-06 23:23:29 +0000

    ok

--- src/libs/floating-box.ts
+++ src/libs/floating-box.ts
@@ -6,29 +6,42 @@ export function spawnFloatingBox(mouseEvent: MouseEvent, ...boxChildren: Templat
 	const component = new FloatingBoxComponent()
 
 	component.$html = html`
-		<div class="backdrop" on:click=${() => component.remove()}></div>
+		<div class="backdrop" on:click=${close}></div>
 		<div class="box">${boxChildren}</div>
 	`
 	document.body.appendChild(component)
 
 	const box = component.$root.querySelector(".box") as HTMLDivElement
 
-	function update() {
-		const rect = box.getBoundingClientRect()
-		if (rect.top < 0) box.style.setProperty("--offset-y", `${-rect.top}px`)
-
-		if (rect.left < 0) box.style.setProperty("--offset-x", `${-rect.left}px`)
-		const right = innerWidth - rect.right
-		if (right < 0) box.style.setProperty("--offset-x", `${right - 16}px`)
-	}
-	component.$interval(update, 100)
 	component.$onMount(() => {
 		const rect = box.getBoundingClientRect()
 		box.style.transform =
 			`translate(calc(${mouseEvent.x - rect.width * 0.5}px + var(--offset-x, 0px)),` + `calc(${mouseEvent.y - rect.height}px + var(--offset-y, 0px)))`
+		updateOffsets()
 
-		update()
+		window.addEventListener("resize", close, { once: true })
+		return () => window.removeEventListener("resize", close)
 	})
+
+	function close() {
+		component.remove()
+	}
+
+	function updateOffsets() {
+		const boxRect = box.getBoundingClientRect()
+		if (boxRect.top < 0) box.style.setProperty("--offset-y", `${-boxRect.top}px`)
+		if (mouseEvent.target instanceof HTMLElement) {
+			const targetRect = mouseEvent.target.getBoundingClientRect()
+			console.log(boxRect.bottom, "<", targetRect.top)
+			if (boxRect.bottom > targetRect.top) {
+				box.style.setProperty("--offset-y", `${-(boxRect.bottom - targetRect.top)}px`)
+			}
+		}
+
+		if (boxRect.left < 0) box.style.setProperty("--offset-x", `${-boxRect.left}px`)
+		const right = innerWidth - boxRect.right
+		if (right < 0) box.style.setProperty("--offset-x", `${right - 16}px`)
+	}
 
 	return component
 }
