3e3aefbca30ab54255efba4cd17920eaceda5083
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-11 17:00:17 +0000

    ...

--- src/import-styles.ts
+++ src/import-styles.ts
@@ -2,10 +2,10 @@ import globalCss from "@/styles/global.css?inline"
 import rootCss from "@/styles/root.css?inline"
 import { Component } from "master-ts/library/component"
 
-const globalSheet = new CSSStyleSheet()
-const rootSheet = new CSSStyleSheet()
+const globalStyleSheet = new CSSStyleSheet()
+const rootStyleSheet = new CSSStyleSheet()
 
-await Promise.all([globalSheet.replace(globalCss), rootSheet.replace(rootCss)])
+await Promise.all([globalStyleSheet.replace(globalCss), rootStyleSheet.replace(rootCss)])
 
-Component.$globalStyleSheets.push(globalSheet)
-document.adoptedStyleSheets.push(rootSheet, globalSheet)
+Component.$globalStyleSheets.push(globalStyleSheet)
+document.adoptedStyleSheets.push(rootStyleSheet, globalStyleSheet)
