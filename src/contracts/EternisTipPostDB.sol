ff1eaa717cbf647ddcab642bbc761c1bf421a01c
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-16 17:47:40 +0000

    ...

--- src/contracts/EternisTipPostDB.sol
+++ src/contracts/EternisTipPostDB.sol
@@ -5,12 +5,12 @@ import "./IEternisPostDB.sol";
 contract EternisTipPostDB is IEternisPostDB {
     uint256 public postIndex;
 
-    event EternisTipPost(uint256 postIndex, address tipTo);
+    event EternisTip(uint256 postIndex, address tipTo);
 
     function post(bytes calldata postData, address payable tipTo) external payable {
-        tipTo.transfer(msg.value);
         emit EternisPost(postIndex, postData);
-        emit EternisTipPost(postIndex, tipTo);
+        emit EternisTip(postIndex, tipTo);
         postIndex++;
+        tipTo.transfer(msg.value);
     }
 }
