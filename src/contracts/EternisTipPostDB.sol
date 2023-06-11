23244f39442489cae339217f3f2ac8559b2d6185
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-06-11 09:07:10 +0000

    ...

--- src/contracts/EternisTipPostDB.sol
+++ src/contracts/EternisTipPostDB.sol
@@ -5,13 +5,17 @@ import "./IEternisPostDB.sol";
 contract EternisTipPostDB is IEternisPostDB {
     uint256 public postIndex;
 
-    mappings(uint256 => address) postIndexToTipAddress;
-    mappings(uint256 => uint256) postIndexToTipValue;
+    struct Tip
+    {
+        address to;
+        uint256 value;
+    }
+
+    mapping(uint256 => Tip) public postIndexToTip;
 
     function post(bytes calldata postData, address payable tipTo) external payable {
         emit EternisPost(postIndex, postData);
-        postIndexToTipAddress[postIndex] = tipTo;
-        postIndexToTipValue[postIndex] = msg.value;
+        postIndexToTip[postIndex] = Tip(tipTo, msg.value);
         postIndex++;
         tipTo.transfer(msg.value);
     }
