eb7cecd416569e8a0b90b3e03334ebfc763c6892
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-07-01 08:41:02 +0000

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { masterTs } from "./node_modules/master-ts-vite-plugin/plugin"
-import { parse } from "./node_modules/master-ts/library/template/parse"
 import typescript from "typescript"
 import { defineConfig } from "vite"
 import { viteSingleFile } from "vite-plugin-singlefile"
+import { masterTs } from "./node_modules/master-ts-vite-plugin/plugin"
+import { parse } from "./node_modules/master-ts/library/template/parse"
 
 export default defineConfig({
 	plugins: [masterTs({ parse, typescript }), viteSingleFile()],
