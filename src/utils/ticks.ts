fd2bb3a4e5dc00ab8c835225326c741ed9c0e178
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-08-28 16:26:50 +0000

    ...

--- src/utils/ticks.ts
+++ src/utils/ticks.ts
@@ -1,30 +1,30 @@
 import { $ } from "master-ts/library/$"
 
-export const oneSecondTick = $.readable((set) => {
+export const oneSecondTick = $.readable<number>((set) => {
 	set(Date.now())
 	const interval = setInterval(() => set(Date.now()), 1000)
 	return () => clearInterval(interval)
 })
 
-export const tenSecondsTick = $.readable((set) => {
+export const tenSecondsTick = $.readable<number>((set) => {
 	set(Date.now())
 	const interval = setInterval(() => set(Date.now()), 10000)
 	return () => clearInterval(interval)
 })
 
-export const oneMinuteTick = $.readable((set) => {
+export const oneMinuteTick = $.readable<number>((set) => {
 	set(Date.now())
 	const interval = setInterval(() => set(Date.now()), 60000)
 	return () => clearInterval(interval)
 })
 
-export const tenMinutesTick = $.readable((set) => {
+export const tenMinutesTick = $.readable<number>((set) => {
 	set(Date.now())
 	const interval = setInterval(() => set(Date.now()), 600000)
 	return () => clearInterval(interval)
 })
 
-export const oneHourTick = $.readable((set) => {
+export const oneHourTick = $.readable<number>((set) => {
 	set(Date.now())
 	const interval = setInterval(() => set(Date.now()), 3600000)
 	return () => clearInterval(interval)
