535cb8806fc3e6d00e52abbd4493b010163a3fa6
Shiba <44804845+DeepDoge@users.noreply.github.com>
2024-01-22 21:32:44 +0000

    Fix ethereum provider check in getSigner function

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -2,11 +2,12 @@ import { BrowserProvider, Eip1193Provider } from "ethers"
 
 declare global {
 	interface Window {
-		ethereum: Eip1193Provider
+		ethereum?: Eip1193Provider
 	}
 }
 
 export async function getSigner() {
+	if (!window.ethereum) throw new Error("No ethereum provider")
 	const browserProvider = new BrowserProvider(window.ethereum)
 	return await browserProvider.getSigner()
 }
