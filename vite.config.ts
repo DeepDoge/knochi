0471189340911623d5a8d9fe6fc814d46df8b78b
Shiba <44804845+DeepDoge@users.noreply.github.com>
2024-01-22 21:01:02 +0000

    Add vite-plugin-singlefile to package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 // vite config
 import { defineConfig } from "vite"
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
+	plugins: [viteSingleFile()],
 	build: {
 		target: "esnext",
 		outDir: "dist",
