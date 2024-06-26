20275cd2c0589848dadf880c9041c86a9f3c8925
Shiba <44804845+DeepDoge@users.noreply.github.com>
2024-06-26 11:13:09 +0000

    something like this

--- modules/service/src/db.ts
+++ modules/service/src/db.ts
@@ -1,36 +1,55 @@
 import { DB } from "@modules/database";
-import { getAddress, isAddress } from "ethers";
+import { getAddress, isAddress, isHexString } from "ethers";
 import { bigint, object, string } from "zod";
 
 const Address = string().refine(isAddress, "Invalid EVM address").transform(getAddress);
-type Address = `0x${any}${any}`;
+type Address = (typeof Address)["_output"];
+
+const Bytes32 = string().refine(
+	(value): value is typeof isHexString extends (value: any, ...args: any[]) => value is infer U ? U : never =>
+		isHexString(value, 32),
+);
+type Bytes32 = (typeof Bytes32)["_output"];
 
 export const db = DB.create("eternis")
 	.version(1, {
+		Feed: DB.ModelBuilder()
+			.parser(
+				object({
+					indexerContractAddress: Address,
+					feedId: Bytes32,
+					length: bigint(),
+				}).strict().parse,
+			)
+			.key({ keyPath: ["indexerContractAddress", "feedId"] })
+			.build(),
 		PostIndex: DB.ModelBuilder()
 			.parser(
 				object({
 					indexerContractAddress: Address,
+					feedId: Bytes32,
 					index: bigint(),
-					id: bigint(),
-					senderAddress: Address, // Most likely to be a Proxy Contract address
+					postId: bigint(),
+					/** Most likely a `proxyContractAddress` */
+					senderAddress: Address,
 					originAddress: Address,
 					timestamp: bigint(),
 				}).strict().parse,
 			)
-			.key({ keyPath: ["indexerContractAddress", "index"] })
-			.index({ field: ["senderAddress", "id"], options: { unique: true } }) // Sender decides the post ID
+			.key({ keyPath: ["indexerContractAddress", "feedId", "index"] })
+			.index({ field: ["indexerContractAddress", "feedId", "index"], options: {} })
+			.index({ field: ["senderAddress", "postId"], options: { unique: true } }) // Sender decides the post ID
 			.index({ field: "originAddress", options: {} })
 			.build(),
 		Post: DB.ModelBuilder()
 			.parser(
 				object({
 					proxyContractAddress: Address,
-					id: bigint(),
+					postId: bigint(),
 					content: string(),
 				}).strict().parse,
 			)
-			.key({ keyPath: ["proxyContractAddress", "id"] })
+			.key({ keyPath: ["proxyContractAddress", "postId"] })
 			.build(),
 	})
 	.build();
