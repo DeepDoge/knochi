29dd215e342b9f1080a69d452c72b437e8233f00
Shiba <44804845+DeepDoge@users.noreply.github.com>
2024-10-06 15:45:05 +0000

    ok fine

--- project/app/src/app.ts
+++ project/app/src/app.ts
@@ -1,23 +1,66 @@
 import "./styles";
 
 import { tags } from "purify-js";
+import { SearchParamsSignal } from "./features/router/url";
 import { css, scopeCss } from "./utils/style";
 
-const { div, header, main, footer } = tags;
+const { div, header, main } = tags;
+
+const documentScroller = document.scrollingElement ?? document.body;
+
+export const menuSearchParam = new SearchParamsSignal("menu");
 
 function App() {
-	return div().id("app").use(scopeCss(AppCss)).children(header(), main());
+	let mainScrollActive = true;
+
+	let mainElement: HTMLElement;
+
+	return div()
+		.id("app")
+		.use(scopeCss(AppCss))
+		.onscrollend((event) => {
+			const currentTarget = event.currentTarget;
+			currentTarget.scrollTo({
+				left: menuSearchParam.val ? 0 : currentTarget.clientWidth,
+				behavior: "smooth",
+			});
+		})
+		.onscroll((event) => {
+			const scrollProgress = Math.min(1, event.currentTarget.scrollLeft / event.currentTarget.clientWidth);
+
+			menuSearchParam.val = scrollProgress < 0.5 ? "open" : null;
+
+			const atActivationFrame = scrollProgress === 1;
+			if (atActivationFrame) {
+				if (mainScrollActive) return;
+				const scrollY = mainElement.scrollTop;
+				mainElement.style.overflow = "visible";
+				mainElement.style.blockSize = "";
+				documentScroller.scrollTop = scrollY;
+				mainScrollActive = true;
+			}
+
+			if (!atActivationFrame) {
+				if (!mainScrollActive) return;
+				const scrollY = documentScroller.scrollTop;
+				mainElement.style.overflow = "hidden";
+				mainElement.style.blockSize = "100dvh";
+				mainElement.scrollTop = scrollY;
+				mainScrollActive = false;
+			}
+		})
+		.children(
+			header().children(new Array(1024).fill("content ")),
+			(mainElement = main().children(new Array(1024).fill("content ")).element),
+		);
 }
 
 const AppCss = css`
 	:scope {
-		container-name: app;
-		container-type: inline-size;
-
-		min-block-size: 100svh;
 		isolation: isolate;
 
 		display: block grid;
+		align-items: start;
 		grid-template-columns:
 			2fr
 			[header-start]
@@ -36,14 +79,31 @@ const AppCss = css`
 				100%
 				[main-end];
 
-			overflow: auto;
-			scroll-snap-type: x mandatory;
-			header {
-				scroll-snap-align: start;
-			}
+			overflow-x: auto;
+			/* 
+				snap breaks randomly if stuff changes at the middle of scrolling.
+				also it would be better if i store the scrolling state on the url anyway
+			*/
+			/* scroll-snap-type: x mandatory; */
+
 			main {
-				scroll-snap-align: end;
+				/* scroll-snap-align: end; */
 			}
+
+			header {
+				/* scroll-snap-align: start; */
+				animation: hide-header-scroll linear;
+				animation-timeline: scroll(x);
+				z-index: -1;
+			}
+		}
+	}
+
+	@keyframes hide-header-scroll {
+		to {
+			scale: 0.95;
+			translate: calc(100% + 1em) 0;
+			opacity: 0;
 		}
 	}
 
@@ -53,16 +113,11 @@ const AppCss = css`
 
 		background-color: red;
 
-		animation: hide-header-scroll linear;
-		animation-timeline: scroll(x);
-		z-index: -1;
-	}
+		position: sticky;
+		inset-block-start: 0;
+		block-size: 100dvh;
 
-	@keyframes hide-header-scroll {
-		to {
-			scale: 0.95;
-			translate: calc(100% + 1em) 0;
-		}
+		overflow: auto;
 	}
 
 	main {
