05f21c89efef6e8fa13e86bfd105410836ffdde7
Shiba <44804845+DeepDoge@users.noreply.github.com>
2024-10-07 13:24:34 +0000

    popover fits here better than dialog, dialog is more like a promise

--- project/app/src/features/wallet/modal.ts
+++ project/app/src/features/wallet/modal.ts
@@ -3,53 +3,48 @@ import { SearchParamsSignal } from "~/features/router/url";
 import { css, scopeCss } from "~/utils/style";
 import { WalletList } from "./WalletList";
 
-const { dialog, div, strong } = tags;
+const { div, strong } = tags;
 
 const styleCss = css`
 	:scope {
-		inline-size: min(100%, 35em);
+		display: block grid;
+		align-content: start;
+
+		inline-size: min(100%, 40em);
+		padding: 4em;
+		gap: 1em;
 
 		background-color: var(--base);
 		color: var(--accent);
 		border: solid color-mix(in srgb, var(--base), var(--accent) 10%);
 	}
 
-	.body {
-		display: block grid;
-		align-content: start;
-	}
-
 	strong {
 		font-size: 2em;
-	}
-
-	.list {
-		padding-block: 2em;
+		&::after {
+			content: ":";
+		}
 	}
 `;
 
-const host = dialog().use(scopeCss(styleCss));
-host.children(
-	div({ class: "body" }).children(
-		strong().textContent("Connect Wallet"),
-		div({ class: "list" }).children(WalletList({ onFinally: () => host.element.close() })),
-	),
+const host = div().use(scopeCss(styleCss)).popover("");
+host.children(strong().textContent("Connect Wallet"), WalletList({ onFinally: () => host.element.hidePopover() }));
+
+const searchParam = new SearchParamsSignal<"open">("connect");
+host.use(() =>
+	searchParam.follow((value) => {
+		if (value === "open") {
+			host.element.showPopover();
+		} else {
+			host.element.hidePopover();
+		}
+	}, true),
 );
-host.onclose((event) => {
-	event.preventDefault();
-	searchParam.val = null;
+host.ontoggle((event) => {
+	if (!(event instanceof ToggleEvent)) return;
+	searchParam.val = event.newState === "open" ? "open" : null;
 });
 
 document.body.append(host.element);
 
-const searchParam = new SearchParamsSignal<"open">("connect");
-searchParam.follow((value) => {
-	if (value === "open") {
-		host.element.showModal();
-	} else {
-		host.element.close();
-	}
-}, true);
-
-export { searchParam as connectWalletSearchParam };
 export const connectWalletShowModalHref = searchParam.toHref("open");
