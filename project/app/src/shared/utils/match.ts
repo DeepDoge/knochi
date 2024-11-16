480ed189e356bcd9ced647b40ccd38912018a76b
Shiba <44804845+DeepDoge@users.noreply.github.com>
2024-11-16 22:09:57 +0000

    i was sleeping but then i realize that i can do it like this. and couldnt sleep without changing it.

--- project/app/src/shared/utils/match.ts
+++ project/app/src/shared/utils/match.ts
@@ -1,4 +1,4 @@
-import { ref, Signal } from "@purifyjs/core";
+import { Signal } from "@purifyjs/core";
 
 export function match<T>(signal: Signal<T>) {
 	return {
@@ -6,19 +6,14 @@ export function match<T>(signal: Signal<T>) {
 			return {
 				then<Then>(thenDerive: (signal: Signal<Extract<T, U>>) => Then) {
 					return {
-						else<Else>(
-							elseDerive: (signal: Signal<Exclude<T, U>>) => Else,
-						): Signal<Then | Else> {
-							return ref<Then | Else>(null!, (set) =>
-								signal.derive(condition).follow((isThen) => {
-									console.log(isThen);
-									if (isThen) {
-										return set(thenDerive(signal as never));
-									} else {
-										return set(elseDerive(signal as never));
-									}
-								}, true),
-							);
+						else<Else>(elseDerive: (signal: Signal<Exclude<T, U>>) => Else) {
+							return signal.derive(condition).derive((isThen) => {
+								if (isThen) {
+									return thenDerive(signal as never);
+								} else {
+									return elseDerive(signal as never);
+								}
+							});
 						},
 					};
 				},
