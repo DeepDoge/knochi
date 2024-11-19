f7be813045c132e920260b5dad67cb70f9e3f956
Shiba <44804845+DeepDoge@users.noreply.github.com>
2024-11-19 09:49:58 +0000

    fix(unroll): unroll logic was wrong, fixed that

--- project/app/src/shared/utils/signals/unroll.ts
+++ project/app/src/shared/utils/signals/unroll.ts
@@ -3,8 +3,9 @@ import { computed, Signal } from "@purifyjs/core";
 export type SignalUnroll<T> = T extends Signal<infer U> ? SignalUnroll<U> : T;
 
 export function unroll<T>(signal: T): SignalUnroll<T>;
-export function unroll(value: unknown) {
+export function unroll(signal: Signal<unknown>) {
 	return computed(() => {
+		let value: unknown = signal;
 		while (value instanceof Signal) {
 			value = value.val;
 		}
